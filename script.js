1729ff750290520f4b423463f73e6b111f9e3d12
anash5119 <anash5119@gmail.com>
2023-06-23 12:34:10 -0700

    Add styling and resizing functionality

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-const container = document.querySelector(".container");
-for( let outerIndex = 1; outerIndex <=16; outerIndex++){
-    const gridSquareContainer = document.createElement("div");
-    //gridSquareContainer.classList.add('gridSquareContainer');
-    gridSquareContainer.id = "gridSquareContainer";
-    container.appendChild(gridSquareContainer);
-    for(let innerIndex = 1; innerIndex <=16; innerIndex++){
-        const gridSquare = document.createElement("div");
-        //gridSquare.classList.add("gridSquare");
-        gridSquare.id = "gridSquare";
-        gridSquareContainer.appendChild(gridSquare);
-    }
+const gridContainerSize = 320;
+
+let sliderOutput = document.getElementById("range");
+let sliderValue = document.getElementById("value");
+console.log(sliderOutput.value);
+sliderValue.innerHTML = sliderOutput.value;
+createGrid(sliderOutput.value);
+sliderOutput.oninput = function(){
+    sliderValue.innerHTML = this.value;
+    
+    const filledContainer = document.querySelector(".container");
+    filledContainer.replaceChildren();
+    createGrid(this.value);
 }
 
-let isBlack = document.getElementById("gridSquare");
-isBlack.addEventListener("mouseOver", () => {
-        //isBlack.style.backgroundColor = "black";
-        console.log(isBlack);
-});
+function createGrid(value){
+    const container = document.querySelector(".container");
+    let gridSquareSize = gridContainerSize/value;
+    let marginSize = (gridContainerSize-gridSquareSize)/(value+1);
+    for( let outerIndex = 1; outerIndex <=value; outerIndex++){
+        const gridSquareContainer = document.createElement("div");
+        //gridSquareContainer.classList.add('gridSquareContainer');
+        gridSquareContainer.id = "gridSquareContainer";
+        container.appendChild(gridSquareContainer);
+        for(let innerIndex = 1; innerIndex <=value; innerIndex++){
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("gridSquare");
+            gridSquare.style.width = `${gridSquareSize}px`;
+            gridSquare.style.height = `${gridSquareSize}px`;
+            //gridSquare.style.margin = `${marginSize}px`;
+            gridSquareContainer.appendChild(gridSquare);
+
+            container.addEventListener("mousedown", () => {
+                //makes squares turn black when you hover over div
+                gridSquare.addEventListener("mouseover", () => {
+                    gridSquare.style.backgroundColor = "white";
+                });
+                
+            });
+        }
+    };
+}
