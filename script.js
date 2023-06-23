1c09050965709fceb2646a66827c0b52de30bf84
anash5119 <anash5119@gmail.com>
2023-06-23 14:02:32 -0700

    Optimize draw function

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
-const gridContainerSize = 320;
+const gridContainerSize = 500;
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
-let sliderOutput = document.getElementById("range");
-let sliderValue = document.getElementById("value");
-console.log(sliderOutput.value);
-sliderValue.innerHTML = sliderOutput.value;
-createGrid(sliderOutput.value);
-sliderOutput.oninput = function(){
-    sliderValue.innerHTML = this.value;
-    
-    const filledContainer = document.querySelector(".container");
-    filledContainer.replaceChildren();
-    createGrid(this.value);
+//gets the input of grid squares from user
+function sliderInput(){
+    let sliderOutput = document.getElementById("range");
+    let sliderValue = document.getElementById("value");
+    sliderValue.innerHTML = sliderOutput.value;
+    createGrid(sliderOutput.value);
+    sliderOutput.oninput = function(){
+        sliderValue.innerHTML = this.value;
+        
+        const filledContainer = document.querySelector(".container");
+        filledContainer.replaceChildren(); //delete grid that's already there
+        createGrid(this.value);
+    }
 }
 
 function createGrid(value){
     const container = document.querySelector(".container");
-    let gridSquareSize = gridContainerSize/value;
-    let marginSize = (gridContainerSize-gridSquareSize)/(value+1);
+    let gridSquareSize = (gridContainerSize/value);
+
+    //loop to make 16 rows
     for( let outerIndex = 1; outerIndex <=value; outerIndex++){
         const gridSquareContainer = document.createElement("div");
         //gridSquareContainer.classList.add('gridSquareContainer');
         gridSquareContainer.id = "gridSquareContainer";
         container.appendChild(gridSquareContainer);
+
+        //loops to fill rows with boxes
         for(let innerIndex = 1; innerIndex <=value; innerIndex++){
             const gridSquare = document.createElement("div");
             gridSquare.classList.add("gridSquare");
             gridSquare.style.width = `${gridSquareSize}px`;
             gridSquare.style.height = `${gridSquareSize}px`;
-            //gridSquare.style.margin = `${marginSize}px`;
-            gridSquareContainer.appendChild(gridSquare);
-
-            container.addEventListener("mousedown", () => {
-                //makes squares turn black when you hover over div
-                gridSquare.addEventListener("mouseover", () => {
-                    gridSquare.style.backgroundColor = "white";
-                });
-                
-            });
+            gridSquare.addEventListener("mousedown", (draw));
+            gridSquare.addEventListener("mouseover", (draw));
+            gridSquareContainer.appendChild(gridSquare);        
         }
-    };
+    }
+}
+
+
+function draw(e){
+    if(e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = "white";
+}
+
+window.onload = () => {
+    sliderInput();
 }
